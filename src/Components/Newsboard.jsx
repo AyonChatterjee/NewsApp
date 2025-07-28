964275ce449a865a721f15ab67a8e274b777883c
Ayon Chatterjee <chatterjeeayon542@gmail.com>
2025-07-28 15:40:54 +0530

    Installed NProgress library

--- src/Components/Newsboard.jsx
+++ src/Components/Newsboard.jsx
@@ -11,19 +11,22 @@ const Newsboard = ({ category, searchTerm }) => {
     // Fetching news articles based on category and search term
 
     const [articles, setArticles] = useState([]);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         const fetchNews = async () => {
             try {
 
+                setLoading(true);
                 NProgress.start(); // start loading bar
 
                 let url = "";
 
                 if (searchTerm.trim() === "") {
-                    url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+                    url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&page=${page}&pageSize=10&apiKey=${import.meta.env.VITE_API_KEY}`;
                 } else {
-                    url = `https://newsapi.org/v2/everything?q=${searchTerm}&sortBy=publishedAt&language=en&apiKey=${import.meta.env.VITE_API_KEY}`;
+                    url = `https://newsapi.org/v2/everything?q=${searchTerm}&sortBy=publishedAt&page=${page}&pageSize=10&language=en&apiKey=${import.meta.env.VITE_API_KEY}`;
                 }
 
                 const response = await fetch(url);
@@ -34,17 +37,17 @@ const Newsboard = ({ category, searchTerm }) => {
                     setArticles([]);
                 }
 
-                NProgress.done() ; // End loading bar
+                NProgress.done(); // End loading bar
             } catch (error) {
                 console.error("Failed to fetch news:", error);
                 setArticles([]);
 
-                NProgress.done() ; // Even an error 
+                NProgress.done(); // Even an error 
             }
         };
 
         fetchNews();
-    }, [category, searchTerm]);
+    }, [page, category, searchTerm]);
 
 
     return (
